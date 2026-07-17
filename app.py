a08d49a07f3f7a8294c426dc2b458d750c2132b8
drsushant-bahekar <drsushantbahekar@gmail.com>
2026-07-17 19:29:03 +0530

    Model Change to 2.5

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ if user_query:
             
             # Call generate_content directly using the client and specify the model
             response = client.models.generate_content(
-                model='gemini-1.5-flash',
+                model='gemini-2.5-flash',
                 contents=prompt 
                 )
 
